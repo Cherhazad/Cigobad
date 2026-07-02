8972ce4a2f6f0c954c1bfcd911188c4c4f1ce3ed
Chérhazad Boustani <160526114+Cherhazad@users.noreply.github.com>
2026-07-02 16:33:12 +0200

    creating dropdown menu for table actions column (#17)

---
+++ front/app/components/CDropdownMenu.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import type {MenuItem} from "primevue/menuitem";
+
+const menu = ref();
+
+const props = defineProps({
+  menuItems: {
+    type: Array as PropType<MenuItem[]>,
+    required: true,
+  },
+  menuIcon: {
+    type: String,
+    default: ''
+  },
+  menuLabel: {
+    type: String,
+    default: ''
+  }
+})
+
+const toggle = (event: MouseEvent): void => {
+  menu.value.toggle(event);
+};
+</script>
+
+<template>
+  <div class="flex justify-center">
+    <Menu ref="menu" :model="menuItems" popup class="w-40"/>
+    <Button
+        type="button" class="min-w-fit" :icon="menuIcon" variant="outlined" severity="secondary"
+        @click="toggle">
+      {{ menuLabel }}
+      <icon :name="menuIcon"/>
+    </Button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
