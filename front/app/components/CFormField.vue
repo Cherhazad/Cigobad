9b8d42c363bf5786f76170a2df354a6b029ff373
Chérhazad Boustani <cherhazadboustani@MacBook-Air-de-Cherhazad.local>
2026-07-06 14:07:23 +0200

    start creating form components + side panel to edit item

---
+++ front/app/components/CFormField.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+
+import {FieldTypes} from "shared";
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: FieldTypes,
+    required: true
+  }
+})
+
+const model = defineModel()
+const emit = defineEmits(["update:modelValue"])
+</script>
+
+<template>
+  <UFormField :label="label" v-bind="$attrs">
+    <UInput
+        v-if="type === FieldTypes.string"
+        v-model="model"
+        v-bind="$attrs"
+        type="string"
+        @update:model-value="emit('update:modelValue', $event)"
+    />
+
+    <UInput
+        v-if="type === FieldTypes.email"
+        v-model="model"
+        type="email"
+        v-bind="$attrs"
+        @update:model-value="emit('update:modelValue', $event)"
+    />
+
+    <UInput
+        v-if="type === FieldTypes.date"
+        v-model="model"
+        type="date"
+        v-bind="$attrs"
+        @update:model-value="emit('update:modelValue', $event)"
+    />
+
+    <UInput
+        v-if="type === FieldTypes.tel"
+        v-model="model"
+        type="tel"
+        v-bind="$attrs"
+        @update:model-value="emit('update:modelValue', $event)"
+    />
+
+    <USelectMenu
+        v-if="type === FieldTypes.select"
+        v-model="model"
+        v-bind="$attrs"
+        @update:model-value="emit('update:modelValue', $event)"
+    />
+
+    <USelectMenu
+        v-if="type === FieldTypes['select-multiple']"
+        v-model="model"
+        v-bind="$attrs"
+        multiple
+        @update:model-value="emit('update:modelValue', $event)"
+    />
+
+    <UTextarea
+        v-if="type === FieldTypes.textarea"
+        v-model="model"
+        v-bind="$attrs"
+    />
+
+    <UCheckbox
+        v-if="type === FieldTypes.checkbox"
+        v-model="model"
+        v-bind="$attrs"
+    />
+
+    <UInputNumber
+        v-if="type === FieldTypes.number"
+        v-model="model"
+        v-bind="$attrs"
+    />
+
+  </UFormField>
+</template>
+
+<style scoped>
+
+</style>
---
+++ shared/src/enums/field-types.enum.ts
@@ -0,0 +1,13 @@
+export enum FieldTypes {
+    'string' = 'string',
+    'number' = 'number',
+    'boolean' = 'boolean',
+    'date' = 'date',
+    'tel' = 'tel',
+    'textarea' = 'textarea',
+    'select' = 'select',
+    'select-multiple' = 'select-multiple',
+    'checkbox' = 'checkbox',
+    'email' = 'email',
+
+}
