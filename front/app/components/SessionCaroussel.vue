27f3a0144fee57d34353611876f4e44c5ea214ee
Chérhazad Boustani <160526114+Cherhazad@users.noreply.github.com>
2026-07-20 20:17:01 +0200

    Subscribe to a session (#28)
    * Session entity + crud
    
    * Session form + sidepanel - Work in progress
    
    ---------

--- front/app/components/SessionCaroussel.vue
+++ front/app/components/SessionCaroussel.vue
@@ -1,23 +1,70 @@
 <script setup lang="ts">
+import {Pencil, Times} from "@primeicons/vue";
+import type {Level} from "shared";
 
+const config = useRuntimeConfig()
+const api = config.public.apiBase
+
+const sidePanel = ref(false)
+
+/*const {data: fetchSessions, refresh} = await useFetch<UserDto[]>(`${api}/user`, {
+  method: 'GET',
+})*/
+
+/*onMounted(async () => {
+  users.value = fetchUsers.value
+})*/
+
+const onEditSession = () => {
+  /*selectedUser.value = {...session}*/
+  sidePanel.value = true
+}
+
+const onSubmitted = async () => {
+  /*refresh()*/
+  /*toast.add({severity: 'success', summary: 'Modifications', detail: 'Modifications enregistrées', life: 3000});*/
+  sidePanel.value = false
+}
+
+const items = [
+  {
+    label: 'Éditer',
+    icon: Pencil,
+    command: () => onEditSession(),
+  },
+  {separator: true},
+  {
+    label: 'Supprimer',
+    icon: Times,
+    class: 'text-red-500! dark:text-red-400!',
+    command: () => console.log('I delete the event'),
+  },
+]
+
+const levels = (level: Level) => {
+  switch (level) {
+    case 'Confirme':
+      return 'Niveau confirmé';
+    case 'Debutant':
+      return 'Niveau débutant';
+    case 'Intermediaire':
+      return 'Niveau intermédiaire';
+  }
+}
 </script>
 
 <template>
   <div class="flex justify-center">
-    <Card class="w-3/5 overflow-hidden" :pt="{ body: { class: 'pt-16!' } }">
+    <Card class="w-3/5 overflow-hidden mt-4">
 
       <template #header>
-        <div class="relative">
-          <img
-              class="w-full max-h-42 object-cover"
-              alt="user header"
-              src="https://images.unsplash.com/photo-1513649718256-1a7162666bad?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          >
-
+        <div class="flex justify-end gap-2 m-4">
+          <Button>
+            M'inscrire
+          </Button>
+          <CDropdownMenu :menu-items="items" menu-icon="i-lucide-ellipsis-vertical"/>
         </div>
-
       </template>
-
 
       <template #title>
         <span class="font-bold text-5xl">Mardi</span>
@@ -41,16 +88,17 @@
           </div>
         </div>
       </template>
-
-      <template #footer>
-        <div class="flex justify-end gap-2 mt-4">
-          <Button>
-            M'inscrire
-          </Button>
-        </div>
-      </template>
     </Card>
   </div>
+
+  <USlideover
+      v-model:open="sidePanel"
+      title="Éditer les informations"
+  >
+    <template #body>
+      <SessionForm @submitted="onSubmitted()"/>
+    </template>
+  </USlideover>
 
 </template>
 
