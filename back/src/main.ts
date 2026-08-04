e0edbfebd42ebc2d2148f28f4e145bb69ca38d13
Chérhazad Boustani <cherhazad.boustani@outlook.fr>
2026-08-04 22:16:14 +0200

    add run migration lines in main.ts

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { DataSource } from 'typeorm';
 
 async function bootstrap() {
   dotenv.config();
@@ -12,6 +13,13 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT ?? 3008);
+
+  const dataSource = app.get(DataSource);
+  const migrations = await dataSource.runMigrations();
+  console.log(
+    'Migrations executées:',
+    migrations.map((m) => m.name),
+  );
 
   console.log('Hey babe! Server well started on port', process.env.PORT);
 }
